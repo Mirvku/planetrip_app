4ad3ad3894a649c0601ebdbc04870eedc51a9bbd
ShinRyuu <83914541+ShinRyuuu@users.noreply.github.com>
2022-02-22 20:24:22 +0700

    Update splash_page.dart

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -21,7 +21,7 @@ class _SplashPageState extends State<SplashPage> {
         Navigator.pushNamedAndRemoveUntil(
             context, '/get-started', (route) => false);
       } else {
-        print(user.email);
+        // print(user.email);
         Navigator.pushNamedAndRemoveUntil(context, '/main', (route) => false);
       }
     });
